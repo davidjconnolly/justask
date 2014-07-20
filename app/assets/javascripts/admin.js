16db72307b00ef3be1aa8d06b4760b3eeff6b0a3
David Connolly <david@twg.ca>
2014-07-20 19:39:22 -0400

    restricted editing on admin surveys to just the selected elements

---
+++ app/assets/javascripts/admin.js
@@ -0,0 +1,17 @@
+window.admin_surveys = {
+  init: function(options) {
+    // scrollAnchor = $('#scrollAnchor')
+    // if (scrollAnchor.length > 0) {
+    //   $(window).scrollTop(scrollAnchor.offset().top);
+    // }
+    // if(window.location.hash !== ''){
+    //   var scrollPos = parseInt(window.location.hash.substring(1),10);
+    //   $(window).scrollTo(scrollPos);
+    // }
+
+    // $( "button.btn" ).on("click", function() {
+    //   var currentScroll = $(window).scrollTop();
+    //   window.location = 'http://example.com#' + currentScroll;
+    // });
+  }
+};
